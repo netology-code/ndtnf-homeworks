c3893f1c121c1ed02445a853f3b6e2f14a9bcf3a
denizen24 <akakiy813@gmail.com>
2021-03-01 08:28:02 +0300

    add rxjs-ts-starter

---
+++ 009-rxjs/src/index.ts
@@ -0,0 +1,9 @@
+import {of, from, timer, range} from 'rxjs';
+
+const o = range(0, 10)
+
+o.subscribe({
+  next: (value: any) => console.log('Next:', value),
+  complete: () => console.log('Complete!'),
+  error: (error) => console.log('Error!', error)
+})
