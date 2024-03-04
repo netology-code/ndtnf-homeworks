48748fef3e1bb5c643cf9f03f9bf5f0e8115c4bc
Olga Stoltz <stoltzoa@gmail.com>
2024-03-04 21:06:40 +0300

    solved task

---
+++ 002-TypeScript/src/app.js
@@ -0,0 +1,8 @@
+"use strict";
+class BooksRepository {
+    createBook(book) { } //— создание книги.  
+    getBook(id) { } //— получение книги по id.
+    getBooks() { } //— получение всех книг.
+    updateBook(id) { } //— обновление книги.
+    deleteBook(id) { } //— удаление книги.
+}
