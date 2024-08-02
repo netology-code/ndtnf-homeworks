a61a9929bf19a4ef0567b6aa32a9ecd2b4322132
Streltsov Ivan <principdereva@gmail.com>
2024-08-02 20:38:36 +0200

    init

--- 009-rxjs/src/rxjs/rxjs.controller.ts
+++ 009-rxjs/src/rxjs/rxjs.controller.ts
@@ -1,13 +1,13 @@
 import { Controller, Get, Query } from "@nestjs/common";
 import { RxjsService } from "./rxjs.service";
-import { IParamText } from "./interfaces/text-param";
 
 @Controller("rxjs")
 export class RxjsController {
   constructor(private rxjsService: RxjsService) {}
 
-  @Get("repositories/")
-  async repositories(@Query() { text, hub }: IParamText) {
-    return await this.rxjsService.searchRepositories(text, hub);
+  @Get('repositories')
+  async getRepositories(@Query('q') query: string): Promise<any> {
+    return this.rxjsService.searchRepositories(query);
   }
+  
 }
--- 009-rxjs/src/rxjs/rxjs.service.ts
+++ 009-rxjs/src/rxjs/rxjs.service.ts
@@ -1,33 +1,40 @@
 import { Injectable } from "@nestjs/common";
-import {
-  firstValueFrom,
-  toArray,
-  from,
-  map,
-  mergeAll,
-  take,
-  Observable,
-} from "rxjs";
+import { firstValueFrom, toArray, from, map, mergeMap, take, Observable } from "rxjs";
 import axios from "axios";
 
 @Injectable()
 export class RxjsService {
-  private readonly githubURL = "https://api.github.com/search/repositories?q=";
+  private readonly githubURL = "https://xeno-canto.org/api/2/recordings?query=";
 
-  private getGithub(text: string, count: number): Observable<any> {
-    return from(axios.get(`${this.githubURL}${text}`))
-      .pipe(
-        map((res: any) => res.data.items),
-        mergeAll(),
-      )
-      .pipe(take(count));
+  private getGithub(q: string, count: number): Observable<any> {
+    console.log(`Fetching data from URL: ${this.githubURL}${q}`); 
+    return from(axios.get(`${this.githubURL}${q}`)).pipe(
+      map((res: any) => {
+        console.log('API Response:', res.data); 
+        if (res.data && res.data.recordings) {
+          return res.data.recordings;
+        } else {
+          throw new Error("Invalid response structure");
+        }
+      }),
+      mergeMap(recordings => from(recordings)),
+      take(count)
+    );
   }
 
-  async searchRepositories(text: string, hub: string): Promise<any> {
-    // Здесь можно добавить логику проверки на какой hub делать запрос
-    console.log("hub = ", hub);
-    const data$ = this.getGithub(text, 10).pipe(toArray());
-    data$.subscribe(() => {});
-    return await firstValueFrom(data$);
+  async searchRepositories(q: string): Promise<any> {
+    if (!q) {
+      throw new Error('Query parameter "q" is required');
+    }
+    console.log(`Searching for: ${q}`);
+    const data$ = this.getGithub(q, 10).pipe(toArray());
+    try {
+      const result = await firstValueFrom(data$);
+      console.log('Search Result:', result);
+      return result;
+    } catch (error) {
+      console.error('Error occurred while fetching repositories:', error);
+      throw error;
+    }
   }
 }
